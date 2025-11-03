c23b74faec34f2825a280609e333a5020bc263ea
Priyabrata Pattanaik <githubprojects100@gmail.com>
2025-11-03 08:36:54 +0530

    Update city check and result message logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // ✅ Required for ngModel
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
 // Variable Declarations
   title = 'Silicon Management Module';
-  name = '4thyearstudent';
-
+  name = '';
+  userName ='';
   stateName : string = 'Odisha';
 
   showSubmitButton : boolean = false;
@@ -22,11 +23,44 @@ export class AppComponent {
 
   makeDisable : boolean = true;
 
+  localPeople : boolean = true;
 
+  cityResult: boolean = false;
   //methods if any
 
-  showMessage(){  
-    alert('Button Clicked Successfully');
+  isSubmitted : boolean = false;
+  city :string = '';
+
+  showMessage(any ?: any){  
+
+    //
+    // if (this.bgColor === 'red')
+    //   {
+    //        this.bgColor = 'blue';
+    //   }
+    //   else{
+    //      this.bgColor = 'red';
+    //   }
+   
+    //You can use Ternary Operator also
+        this.bgColor = this.bgColor === 'red' ? 'blue' : 'red';
+
+  this.name ='Button Clicked!';
+
+
+  }
+
+
+  // Result message for city check
+
+  onSubmit(form: any) {
+    
+    this.isSubmitted = true
+    if (form.value.city  === 'Bhubaneswar') {
+      this.cityResult = true;
+    } else {
+      this.cityResult = false;
+    }
   }
 
 
