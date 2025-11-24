2aaa2499f311ad49aa47bde82d21259c81f94157
Narayan Sahu <128928379+narayan439@users.noreply.github.com>
2025-11-24 23:52:21 +0530

    Change team member name (team 3)

--- src/app/team4/team4.component.ts
+++ src/app/team4/team4.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataserviceService } from '../service/dataservice.service';   
 
 @Component({
   selector: 'app-team4',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./team4.component.css']
 })
 export class Team4Component {
+
+  constructor(private abc: DataserviceService) {  
+    alert("Team4Component is created");
+  }
+
+  ngOnInit(): void {
+    console.log(this.abc.serviceData);
+  }
+  handleClick() {
+  alert("Button clicked!");
+}
+
 
 }
